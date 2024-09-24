e5270925f35eef2550fc9c154a4bdb9c7156934f
priyaaanshh <priyansh.sahu.cer21@itbhu.ac.in>
2024-09-25 01:41:42 +0530

    reverting code back : vercel don't support websockets

--- server/api/index.js
+++ server/api/index.js
@@ -1,33 +1,15 @@
 import express from "express";
-import { setupSocket } from "./socket.js"; // Adjust the path as needed
+import http from "http";
+import { Server } from "socket.io";
+import { setupSocket } from "../socket/socket.js"; 
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-// This is a serverless function in Vercel
-const handler = (req, res) => {
-    // Handle HTTP requests
-    if (req.method === "GET") {
-        return res.send("Express on Vercel");
-    }
+setupSocket(io);
 
-    res.status(405).send("Method Not Allowed");
-};
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.all("*", handler);
-
-// Setting up Socket.IO will require an HTTP server
-export default (req, res) => {
-    // Call the Express handler
-    const expressHandler = app(req, res);
-
-    // If the request is for WebSocket, set up Socket.IO
-    if (req.headers['upgrade'] === 'websocket') {
-        const httpServer = require('http').createServer(app);
-        setupSocket(httpServer);
-
-        // Handle WebSocket connections
-        httpServer.emit('request', req, res);
-    }
-
-    return expressHandler;
-};
+const PORT = 8080;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
