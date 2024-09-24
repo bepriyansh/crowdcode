af6f10bcb0566bab14ea3e0766e85ada27a05856
priyaaanshh <priyansh.sahu.cer21@itbhu.ac.in>
2024-09-25 00:52:20 +0530

    running server on port 3000 for testing deployment

--- server/api/index.js
+++ server/api/index.js
@@ -10,7 +10,7 @@ const io = new Server(server);
 
 setupSocket(io);
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
