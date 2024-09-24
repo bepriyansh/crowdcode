703343d2868fa0abfc6e9924929e2e5d26d492ae
priyaaanshh <priyansh.sahu.cer21@itbhu.ac.in>
2024-09-25 00:58:46 +0530

    testing with template

--- server/api/index.js
+++ server/api/index.js
@@ -1,16 +1,5 @@
-// server.js
 import express from "express";
-import http from "http";
-import { Server } from "socket.io";
-import { setupSocket } from "../socket/socket.js"; 
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-setupSocket(io);
-
 app.get("/", (req, res) => res.send("Express on Vercel"));
-
-const PORT = 3000;
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(3000, () => console.log("Server ready on port 3000."));
