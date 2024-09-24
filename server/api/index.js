a75318a61d222f96bd13c5149e87627807e7cf1c
priyaaanshh <priyansh.sahu.cer21@itbhu.ac.in>
2024-09-25 01:07:32 +0530

    added socketio in main index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { setupSocket } from "../socket/socket.js"; 
 
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+setupSocket(io);
+
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.listen(3000, () => console.log("Server ready on port 3000."));
