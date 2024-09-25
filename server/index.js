532e438e34880fd7228d320c38eedc1937ba3229
priyaaanshh <priyansh.sahu.cer21@itbhu.ac.in>
2024-09-25 21:37:49 +0530

    deploying server on render

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
-import { setupSocket } from "../socket/socket.js"; 
+import { setupSocket } from "./socket/socket.js"; 
 
 const app = express();
 const server = http.createServer(app);
