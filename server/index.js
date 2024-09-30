7e79f4c0f646e4a1a7232ff295aff998166feb72
priyaaanshh <priyansh.sahu.cer21@itbhu.ac.in>
2024-09-30 08:46:11 +0530

    added keepalive ping in server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
-import { setupSocket } from "./socket/socket.js"; 
+import { setupSocket } from "./socket/socket.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -10,6 +10,11 @@ const io = new Server(server);
 setupSocket(io);
 
 app.get("/", (req, res) => res.send("Express on Render"));
+app.get("/keepalive", (req, res) => {
+  const time = new Date(Date.now()).toLocaleString();
+  console.log(time, "Keep Alive Ping");
+  res.send(`${time} : Hello from CrowdCode`);
+});
 
 const PORT = 8080;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
